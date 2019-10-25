a850b6317f1814e4f2006af7fd7737e69944aeee
2016405A703 <42986208+2016405A703@users.noreply.github.com>
2019-10-25 14:59:33 +0800

    Add files via upload

--- RabbitMQ/src/test/java/com/example/demo/DemoApplicationTests.java
+++ RabbitMQ/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes= DemoApplication.class)
+@SpringBootTest()
 public class DemoApplicationTests {
     @Test
     public void contextLoads() {
